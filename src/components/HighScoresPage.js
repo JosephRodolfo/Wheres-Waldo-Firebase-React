f11784677d6e87f9271ad4db9efb1547d4dd66ff
JosephRodolfo <38168806+JosephRodolfo@users.noreply.github.com>
2022-04-12 14:36:39 -0400

    Add handling for no high scores on high scores page

--- src/components/HighScoresPage.js
+++ src/components/HighScoresPage.js
@@ -5,22 +5,31 @@ import underConstruction from "../assets/images/skeleton.jpg";
 import { HighScoreCard } from "./HighScoreCard";
 
 export const HighScoresPage = (props) => {
+  if (props.highScoresDataTwo.length !== 0) {
+    localStorage.setItem("scores", JSON.stringify(props.highScoresDataTwo));
+  }
 
- if (props.highScoresDataTwo.length !==0){localStorage.setItem("scores", JSON.stringify(props.highScoresDataTwo))};
-
-   JSON.parse(localStorage.getItem("scores"));
-  
+  JSON.parse(localStorage.getItem("scores"));
 
   return (
     <div>
       <HeaderNav />
       <h1>High Scores!</h1>
 
-      {JSON.parse(localStorage.getItem("scores")).map((element, index) => {
-        return (
-          <HighScoreCard playerName={element.name} key={index} number={index} score={element.score} />
-        );
-      })}
+      {props.highScoresDataTwo.length === 0 ? (
+        <div>No high scores yet!</div>
+      ) : (
+        JSON.parse(localStorage.getItem("scores")).map((element, index) => {
+          return (
+            <HighScoreCard
+              playerName={element.name}
+              key={index}
+              number={index}
+              score={element.score}
+            />
+          );
+        })
+      )}
     </div>
   );
 };
